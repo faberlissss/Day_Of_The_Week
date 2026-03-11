6ebdf86a391d4ecbba0c2a92def1757122ef84ea
faberlissss <maria.simochka@gmail.com>
2026-03-11 19:25:59 +0200

    Program code added

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Enter the number of the day of the week: ");
+string? userNum = Console.ReadLine();
+
+int.TryParse(userNum, out int numDayOfTheWeek);
+
+switch (numDayOfTheWeek)
+{
+    case 1:
+        Console.Write("Monday");
+        break;
+    case 2:
+        Console.Write("Tuesday");
+        break;
+    case 3:
+        Console.Write("Wednesday");
+        break;
+    case 4:
+        Console.Write("Thursday");
+        break;
+    case 5:
+        Console.Write("Friday");
+        break;
+    case 6:
+        Console.Write("Saturday");
+        break;
+    case 7:
+        Console.Write("Sunday");
+        break;
+    default:
+        Console.Write("You entered an incorrect number");
+        break;
+}
